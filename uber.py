ca4613c40fc152395ec4c1e427f7346b53a3f28d
Chris Rodowicz <crodowicz@college.harvard.edu>
2016-12-03 11:42:32 -0500

    test commit

--- uber.py
+++ uber.py
@@ -1,18 +1,27 @@
-#import node
-import util
+import node
+import passenger
+# import util
 
-class uber:
 
-	# init values
-	def __init__(self, carId, posX = 0, posY = 0, carryingPassenger = false, currentNode = Node(), currentTravelCost = 0):
+class Uber:
+
+	# init values - don't think x,y is needed for car if we do it in node
+	# used passenger
+	def __init__(self, carId, passengerCount = 0, currentNode = Node(), destinationNode = Node(), currentTotalTravelCost = 0):
 		self.carId = carId
-		self.posX = posX
-		self.posY = posY
-		self.carryingPassenger = carryingPassenger
+		self.passengerCount = passengerCount
 		self.currentNode = currentNode
-		self.currentTravelCost = currentTravelCost
+		self.destinationNode = destinationNode
+		self.currentTotalTravelCost = currentTotalTravelCost
 
-	def reachedDestination(self):
+	def pickupPassenger(self, passenger):
+		self.destinationNode = passenger.destinationNode
+		self.currentTotalTravelCost = 0
+
+	def reachedDestination(self, passenger):
+		if self.currentNode == self.destinationNode:
+			passengerCount -= 1
+			return self.currentNode
 
 
 
@@ -20,17 +29,16 @@ class uber:
 
 
 
+	# def graphSearch(problem, frontier):
 
-	def graphSearch(problem, frontier):
+	# def heuristic(a, b):
+	# 	return abs(a.x - b.x) + abs(a.y - b.y)
 
-	def heuristic(a, b):
-		return abs(a.x - b.x) + abs(a.y - b.y)
-
-	# we have a graph setup right? nodes and edges
-	def aStarSearch(problem, heuristic = nullheuristic):
-		frontier = PriorityQueue()
-		frontier.push(start)
-		visited = {}
-		visited[start] = True
-		return frontier
+	# # we have a graph setup right? nodes and edges
+	# def aStarSearch(problem, heuristic = nullheuristic):
+	# 	frontier = PriorityQueue()
+	# 	frontier.push(start)
+	# 	visited = {}
+	# 	visited[start] = True
+	# 	return frontier
 
