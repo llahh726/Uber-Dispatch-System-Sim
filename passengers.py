ad1676a3ba11b951a34bdd8612d935fc3e7d06e4
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-12-03 11:14:52 -0500

    spawning

--- passengers.py
+++ passengers.py
@@ -1,14 +1,35 @@
-ID_INDEX = 0
+import random
+
+MAX_X = 10
+MAX_Y = 10
 
 class Passenger:
-    def __init__(self, xy1, xy2, num=0):
-        self.start = xy1
-        self.goal = xy2
+    def __init__(self, node1, node2, idInt,  num=0):
+        self.start = node1
+        self.goal = node2
         self.time = num
         self.pickedUp = False
         # keep track of which Uber picked passenger up or not?
         self.route = [self.start]
 
-        self.ID = ID_INDEX
+        self.ID = idInt
+
+    def info(self):
+        return [self.ID, self.start, self.goal, self.time, self.pickedUp, self.route]
+
+def spawn():
+    # work in progress: where do I get the coordinates?
+    passengers = []
+    ID_INDEX = 0
+    # so nodes would actually have to be made first
+    for i in xrange(10):
+        passengers.append(Passenger(random.choice(nodes), random.choice(nodes), ID_INDEX))
         ID_INDEX += 1
+
+    for p in passengers:
+        print p.info()
+
+def main():
+    spawn()
+main()
 
