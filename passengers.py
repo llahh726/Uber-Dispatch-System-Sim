6499b147aa69e3e7df98c644085af00e5fc1d6fc
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-12-03 11:16:09 -0500

    spawn(n)

--- passengers.py
+++ passengers.py
@@ -17,12 +17,13 @@ class Passenger:
     def info(self):
         return [self.ID, self.start, self.goal, self.time, self.pickedUp, self.route]
 
-def spawn():
+# n = num to spawn
+def spawn(n):
     # work in progress: where do I get the coordinates?
     passengers = []
     ID_INDEX = 0
     # so nodes would actually have to be made first
-    for i in xrange(10):
+    for i in xrange(n):
         passengers.append(Passenger(random.choice(nodes), random.choice(nodes), ID_INDEX))
         ID_INDEX += 1
 
@@ -30,6 +31,6 @@ def spawn():
         print p.info()
 
 def main():
-    spawn()
+    spawn(10)
 main()
 
