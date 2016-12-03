c7c0f38543a4f9ae731505ac9dfede89d728b6ab
llahh726 <llahh726@gmail.com>
2016-12-03 14:58:50 -0500

    modified graph+nodes

--- nodes.py
+++ nodes.py
@@ -8,6 +8,7 @@ Node class for Uber Dispatch System
 Node -> intersection of rodes
 '''
 import numpy as np
+NODE_ID = 0
 
 cclass Node:
     # init values
@@ -18,25 +19,12 @@ cclass Node:
         self.traffic = traffic
         self.neighbors = neighbors
         self.passengers = passengers
+        global NODE_ID
+        NODE_ID += 1
     
     # return neighbors
     def get_neighbors(self):
         return self.neighbors
-    
-    
-    # add neighbors (doesn't have to be mutual, could be one way)
-    def add_neighbor(self, neighbor):
-        if neighbor in self.neighbors:
-            print 'They are already neighrbors!'
-        else:
-            self.neighbors.append(neighbor)
-            for i in self.neighbors:
-                print i.node_id, '1'
-            neighbor.neighbors.append(self)
-            for j in neighbor.neighbors:
-                print j.node_id, '2'
-            if self in self.neighbors:
-                self.neighbors.remove(self)
     
     # coordinates
     def get_coord(self):
@@ -81,20 +69,3 @@ def add_neighbor(node1, node2):
     else:
         node1.neighbors.append(node2)
         node2.neighbors.append(node1)
-        
-    
-
-if __name__ == '__main__':
-    aa = Node(node_id=10)
-    bb = Node(node_id=11, x=12, y=50)
-    cc = Node(node_id=12, x=111, y=3)
-    add_neighbor(aa, bb)
-    for i in aa.neighbors:
-        print i.node_id
-    for i in cc.neighbors:
-        print i.node_id
-    print aa.get_euc_dist(cc)
-    aa.add_passenger('a')
-    print 'aa passengers: ', aa.passenters
-    print 'bb passengers: ', bb.passenters
-    print 'cc passengers: ', cc.passenters
