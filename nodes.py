bfbc5c0d2575fd9db6fc16ef00c387c0365e668b
llahh726 <llahh726@gmail.com>
2016-12-03 12:22:46 -0500

    updated nodes

--- nodes.py
+++ nodes.py
@@ -7,9 +7,11 @@ Node class for Uber Dispatch System
 
 Node -> intersection of rodes
 '''
+import numpy as np
+
 class Node(object):
     # init values
-    def __init__(self, node_id, x=0, y=0, traffic=1, neighbors=set(), passengers=[]):
+    def __init__(self, node_id, x=0, y=0, traffic=1, neighbors=[], passengers=[]):
         self.node_id = node_id
         self.x = x
         self.y = y
@@ -23,16 +25,28 @@ class Node(object):
     
     # add neighbors (doesn't have to be mutual, could be one way)
     def add_neighbor(self, neighbor):
-        if neighbor not in self.neighbors:
-            self.neighbors.add(neighbor)
+        if neighbor in self.neighbors:
+            print 'They are already neighrbors!'
+        else:
+            self.neighbors.append(neighbor)
+            neighbor.neighbors.append(self)
+            if self in self.neighbors:
+                self.neighbors.remove(self)
     
     # coordinates
     def get_coord(self):
         return (self.x, self.y)
     
     # get passengers
-    def get_passengers(self):
+    def get_passengers(self):         
         return self.passengers
+    
+    # remove passengers (for pick up)
+    def remove_passenger(self, passenger):
+        if passenger not in self.passengers:
+            print 'Error: passenger not found'
+        else:
+            self.passengers.remove(passenger)
     
     # add passengers
     def add_passenger(self, passenger):
@@ -53,5 +67,28 @@ class Node(object):
         b = np.array([node.x, node.y])
         return np.sqrt(np.sum((a-b)**2))
     
+    
+# no one-ways! roads will be two-ways always
+# use this to add node or use 
+def add_neighbor(node1, node2):
+    if node1 in node2.neighbors:
+        print 'They are already neighrbors!'
+    else:
+        node1.neighbors.append(node2)
+        node2.neighbors.append(node1)
+    
 
-		
+if __name__ == '__main__':
+	aa = Node(node_id=10)
+	bb = Node(node_id=11, x=12, y=50)
+	cc = Node(node_id=12, x=111, y=3)
+	add_neighbor(aa, bb)
+	for i in aa.neighbors:
+		print i.node_id
+	for i in cc.neighbors:
+ 		print i.node_id
+  	print aa.get_euc_dist(cc)
+  	aa.add_passenger('a')
+  	print 'aa passengers: ', aa.passenters
+  	print 'bb passengers: ', bb.passenters
+  	print 'cc passengers: ', cc.passenters
