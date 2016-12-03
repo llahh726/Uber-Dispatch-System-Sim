b9d692b455e6a21271bfabdd6146efa3cd7ac118
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-12-03 11:24:03 -0500

    switched up order to make more sense

--- passengers.py
+++ passengers.py
@@ -1,4 +1,5 @@
 import random
+ID_INDEX = 0
 
 class Passenger:
     def __init__(self, node1, node2, idInt,  num=0):
@@ -15,10 +16,10 @@ class Passenger:
         return [self.ID, self.start, self.goal, self.time, self.pickedUp, self.route]
 
 # n = num to spawn
-def spawn(n):
+def spawn(n, nodes):
+    global ID_INDEX
     # work in progress: where do I get the coordinates?
     passengers = []
-    ID_INDEX = 0
     # so nodes would actually have to be made first
     for i in xrange(n):
         passengers.append(Passenger(random.choice(nodes), random.choice(nodes), ID_INDEX))
@@ -28,6 +29,7 @@ def spawn(n):
         print p.info()
 
 def main():
-    spawn(10)
+    nodes = ['a', 'b', 'c', 'd', 'e'] # random stuff just to get started
+    spawn(10, nodes)
 main()
 
