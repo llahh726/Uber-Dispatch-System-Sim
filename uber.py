0ba6f76c2ab018b2dc04bb125b102c8a56ca7d3e
llahh726 <llahh726@gmail.com>
2016-12-06 16:17:56 -0500

    getting new stuff.. accidentally changed uber

--- uber.py
+++ uber.py
@@ -1,7 +1,5 @@
 from nodes import Node
 from passengers import Passenger
-import math
-
 # import util
 UBER_ID = 0
 
@@ -9,13 +7,10 @@ class Uber:
 
 	## init values - don't think x,y is needed for car if we do it in node
 	## used passenger
-	def __init__(self, carId, passengerCount, passengers, x, y, nodePath, currentNode, destinationNode, currentTotalTravelCost):
+	def __init__(self, carId, passengerCount, passengers, currentNode, destinationNode, currentTotalTravelCost):
 		self.carId = carId # int starting from 0
 		self.passengerCount = passengerCount # int = 0, used count if we maybe do groups later
 		self.passengers = passengers # array of passengers in car
-		self.x = x # x coord
-		self.y = y # y coord
-		self.nodePath = nodePath
 		self.currentNode = currentNode # current Node() that car is at
 		self.destinationNode = destinationNode # Node() that is Passenger() goal
 		self.currentTotalTravelCost = currentTotalTravelCost # int starting at 0 on pickup
@@ -25,7 +20,7 @@ class Uber:
 	def pickupPassenger(self, passenger):
 		if self.currentNode == passenger.start:	
 			# print "There is a passenger here at", (self.currentNode.x, self.currentNode.y)
-			print "Picked up passenger with ID:", passenger.ID
+			# print "Picked up passenger with ID:", passenger.ID
 			self.passengers.append(passenger.ID)
 			# print "Current Passenger list:", self.passengers
 			self.destinationNode = passenger.goal
@@ -40,58 +35,8 @@ class Uber:
 			# print "Passenger wait time:", self.currentTotalTravelCost
 
 		else:
+			## run a* to get there
 			print "No passenger here to pick up"
-
-	# holding off on deleting this so far, but I think it's sufficient to put into graphs.py
-	def travelToPassengerToPickup(self, node):
-		# receive node location of passenger to pickup
-		a_star_search()
-
-	# Gets called at every time step
-	def setNodePath(self):
-		self.nodePath = reconstructPath(a_star_search(self.currentNode, self.destinationNode)[0], self.currentNode, self.destinationNode)
-
-	# In graph, for all ubers:
-	# Each time step is 1. Adds 1 to total travel cost
-	# Needs to be passed a node path
-	def uberMove(self):
-		if self.destinationNode != None:
-			targetNode = self.nodePath[0]
-			print "My coords:", self.x, self.y
-			print "Target coords:", targetNode.x, targetNode.y
-			dx = targetNode.x - self.x
-			dy = targetNode.y - self.y
-			print "Dx:", dx
-			print "Dy:", dy
-			c = math.sqrt(dx**2 + dy**2)
-			print "C:", c
-
-			theta = math.atan(dy / dx)
-			print "Theta:", theta
-			moveY = math.sin(theta)
-			moveX = math.cos(theta)
-			print "Move X:", moveX
-			print "Move Y:", moveY
-			print "Move total", math.sqrt(moveX**2 + moveY**2)
-			if c <= 1.0:
-				self.x = targetNode.x
-				self.y = targetNode.y
-				# move to nextnode in path
-				moveToNextTargetNode()
-				reachedDestination()
-				print "Distance less than 1, reached node and switched to new target"
-			else:
-				self.x += moveX
-				self.y += moveY
-			print "New x:", self.x
-			print "New y:", self.y
-
-		self.currentTotalTravelCost += 1
-		print "-------------------------"
-
-	def moveToNextTargetNode(self):
-		if len(self.nodePath) > 0:
-			self.nodePath.pop(0)
 
 	## NEEDS TO BE CALLED At each time step,
 	## For all cars that have 1 or more passengers,
@@ -99,27 +44,9 @@ class Uber:
 	def reachedDestination(self):
 		if self.currentNode == self.destinationNode:
 			self.passengerCount -= 1
-			self.destinationNode = None
+			# return true
 			# print "Reached destination, dropped off passenger:", self.passengers[0], "at", (self.currentNode.x, self.currentNode.y)
 			# print "Total time:", self.currentTotalTravelCost
-			finishedID = self.passengers[0]
-			del self.passengers[0]
-			return finishedID
-		else:
-			return -1
-
-	# Need to get cost at first step
-
-	def getPathAndCostToDestination(self, currentNode, destinationNode):
-		tuple = graph.a_star_search(currentNode, destinationNode)
-		path = tuple[0]
-		cost = tuple[1]
-		print path
-		print cost
-		# path is the path of nodes to destination
-		# do we move one node per time step? 
-
-
 
 	def getCarId(self):
 		return self.carId
@@ -140,27 +67,33 @@ class Uber:
 		return self.currentTotalTravelCost
 
 
+
+
+	# def graphSearch(problem, frontier):
+
+	# def heuristic(a, b):
+	# 	return abs(a.x - b.x) + abs(a.y - b.y)
+
+	# # we have a graph setup right? nodes and edges
+	# def aStarSearch(problem, heuristic = nullheuristic):
+	# 	frontier = PriorityQueue()
+	# 	frontier.push(start)
+	# 	visited = {}
+	# 	visited[start] = True
+	# 	return frontier
+
 ##############################################################
 # Testing
 
 # Init
-node1 = Node(node_id=1, neighbors=[], passengers=[], x=0.0, y=0.0, traffic=1)
-node2 = Node(node_id=2, neighbors=[node1], passengers=[], x=1.0, y=1.0, traffic=1)
-
-car1 = Uber(carId=1, passengerCount=0, passengers=[], x=0.0, y=0.0, currentNode=node1, destinationNode=None, currentTotalTravelCost=0)
+node1 = Node(0, [], [], 0, 0, 1)
+node2 = Node(1, [node1], [], 1, 1, 1)
+car1 = Uber(1, 0, [], node1, None, 0)
 passenger1 = Passenger(node1, node2, 13, 5)
 
-print passenger1.goal.node_id
 
 car1.pickupPassenger(passenger1)
-print car1.destinationNode.node_id
 # print "Took a time step"
 ## Moved to node2
-#car1.currentNode = node2
-car1.uberMove(car1.destinationNode)
-car1.uberMove(car1.destinationNode)
-
-
+car1.currentNode = node2
 car1.reachedDestination()
-
-#car1.uberMove(node2)
